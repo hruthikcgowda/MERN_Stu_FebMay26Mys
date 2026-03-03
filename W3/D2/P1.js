0d7fdd843a3ffec9b5413b551f921536a19a0773
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-03 15:40:48 +0530

    arithmetic operators

---
+++ W3/D2/P1.js
@@ -0,0 +1,14 @@
+// arithmetic operators
+console.log("arithmetic operators");
+const a=2, b=3;
+let c=0;
+console.log("a+b",c=a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b); //modulus 
+console.log("a**b",a**b); //exponential
+console.log("a++",a++); //post increment
+console.log("++a",++a);  //pre increment
+console.log("a--",a--);  //post decrement
+console.log("--a",--a);  //pre decrement
