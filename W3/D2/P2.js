3b19f381be4870d0d4a6e4dc0da4c8ef16f1be8d
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-03 15:41:17 +0530

    Comparison operators

---
+++ W3/D2/P2.js
@@ -0,0 +1,12 @@
+// Comparison operators
+
+console.log("Comparison operators");
+var x=3, y='3';
+console.log("x==y: ",x==y); //loose equality
+console.log("x===y: ",x===y); //strick equality
+console.log("x!=y: ",x!=y); //loose inequality
+console.log("x!==y: ",x!==y);  //strick inequality
+console.log("x>3: ",x>3);
+console.log("x<3: ",x<3);
+console.log("x>=3: ",x>=3);
+console.log("x<=y: ",x<=y);    
