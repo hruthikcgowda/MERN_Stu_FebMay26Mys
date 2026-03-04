0104f4db61b1bae481c0a3df584e2c8c9f102a88
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-04 13:38:26 +0530

    step size and two counters in a loop

---
+++ W3/D3/P4.js
@@ -0,0 +1,9 @@
+// 1. step size
+for(let i=0;i<=10;i+=3){
+    // console.log("i =",i);
+}
+
+//1. two counters in one for loop
+for(let i=1,j=5;i<=5 && j>=1;i++,j--){
+    console.log("i=",i,"j=",j);
+}
