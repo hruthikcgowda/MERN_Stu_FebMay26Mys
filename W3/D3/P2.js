667f0d8e8f6ec3c0c8530bd8a4fda024691917cb
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-04 13:36:03 +0530

    while and do while loop

---
+++ W3/D3/P2.js
@@ -0,0 +1,16 @@
+// while loop
+console.log("While loop example");
+
+let count = 0;
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
+
+// do while
+let value = 0;
+do {
+console.log("value: ",value);
+    value++;
+}
+while(value<3)
