480a3b469c07aa82b1a53f92941dc8b9032c619a
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-12 12:49:54 +0530

    async fetches

--- W4/D3/P10.js
+++ W4/D3/P10.js
@@ -1,19 +1,19 @@
 const asyncfetchBtn = document.getElementById("asyncfetchBtn");
 const output = document.getElementById("output");
 
-asyncfetchBtn.addEventListener("click",async function(){
-    try{
+asyncfetchBtn.addEventListener("click", async function () {
+    try {
         const response = await fetch("https://jsonplaceholder.typicode.com/users");
-        if(!response.ok)  throw new Error("HTTP Error: "+response.status);
-            const users = await response.json();
-            console.log(users);
-            
-            const firstThree = users.slice(0,3);
-            output.textContent = JSON.stringify(firstThree,null,2);
+        if (!response.ok) throw new Error("HTTP Error: " + response.status);
+        const users = await response.json();
+        console.log(users);
 
-    } 
-    
-    catch(error){
-        output.textContent = "Error: "+error.message;
+        const firstThree = users.slice(0, 3);
+        output.textContent = JSON.stringify(firstThree, null, 2);
+
+    }
+
+    catch (error) {
+        output.textContent = "Error: " + error.message;
     }
 });
