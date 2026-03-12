87585cf82fa6759c09e204368c300c0356ee937d
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-12 11:47:10 +0530

    async fetch

---
+++ W4/D3/P8.js
@@ -0,0 +1,15 @@
+const asyncfetchBtn = document.getElementById("asyncfetchBtn");
+const output = document.getElementById("output");
+
+asyncfetchBtn.addEventListener("click",async function(){
+    output.textContent = "Loading user...";
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts/101");
+        if(!response.ok) throw new Error("HTTP error:"+response.status);
+        const data = await response.json();
+        output.textContent = JSON.stringify(data,null,2);
+    }
+    catch(error){
+        output.textContent = "Error: "+error.message;
+    }
+});
