bec8977ba5dde87aa506844433ededa83e81591c
hruthikcgowda <hruthikcgowda456@gmail.com>
2026-03-22 16:23:08 +0530

    Portfolio certificate

---
+++ W4/D5/portfolio-project/js/data/Certificate.js
@@ -0,0 +1,14 @@
+const certificates = [
+    {
+        title: "MERN Stack Development",
+        issuer: "Udemy",
+    },
+    {
+        title: "JavaScript Basics",
+        issuer: "Coursera",
+    },
+    {
+        title: "HTML & CSS",
+        issuer: "Internship Training",
+    }
+];
